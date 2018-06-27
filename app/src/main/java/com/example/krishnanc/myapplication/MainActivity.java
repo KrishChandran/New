b54b2f4b243f64f2d301486b9539c9e6cb94442e
Chandran <krishnanc@telenav.com>
2018-06-27 06:28:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/krishnanc/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.krishnanc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Initia(){
+        int i=2+1;
+    }
+
+}
